634645f851afdd8a8142094b358d7775ab336406
yluo <luoyi1@yonghui.cn>
2023-03-28 11:04:35 +0800

    opt: clean code

--- bt/indicators/pvolume.py
+++ bt/indicators/pvolume.py
@@ -43,9 +43,7 @@ class BAVGV(Indicator):
 class VPR(Indicator):
     
     lines = ('vpr', )
-    
-    params = (('br', 0.9), ('sr', 0.8), )
-    
+        
     def __init__(self):
         self.lines.vpr = self.data.close / self.data.volume
         
@@ -55,53 +53,23 @@ class PVVP(Indicator):
     
     lines = ('pvvpb', 'pvvps', 'vsma')
     
-    params = (('period', 90), ('sr', 0.9), ('br', 0.8), )
+    params = (('period', 90), ('sr', 0.9), ('br', 0.8), ('rr', 2))
     
     def __init__(self):
         
-        self.mmvp = MMVP(self.data, period=self.p.period)
-        self.pv = MMV(self.data, period=self.params.period)
-        self.vpr = VPR(self.data, sr=self.params.sr, br=self.params.br)
+        self.mmvp = MMVP(period=self.p.period)
+        self.pv = MMV(period=self.params.period)
+        self.vpr = VPR()
         self.lines.vsma = self.data.close / bt.indicators.MovingAverageSimple(self.data.volume, period=10) * self.data.volume
-                
-        # self.lines.pvvpb = pv.lines.minvolume * vpr.lines.vpr
-        
-        # self.lines.pvvps = pv.lines.maxvolume * vpr.lines.vpr
-        # self.lines.mmvp = self.mmvp.lines.minvp
-        
-        # self.sigb[0] = False#(self.pv.lines.minvolume[0] * self.vpr.lines.vpr[0] >= self.data.close[0]) and (self.mmvp.lines.minvp[0] >= self.data.close[0])
-        
-        # self.lines.pvvpb = self.data.close if self.sigb else 0
-        
-        # self.lines.pvvps = self.data.close >= (self.pv.lines.maxvolume * self.vpr.lines.vpr * self.p.sr)
-        
-        # vpr = self.data.close / self.data.volume
-        
-        # self.lines.pvvpb = pv.lines.minvolume * self.data.close / self.data.volume
-        
-        # self.lines.pvvps = pv.lines.maxvolume * self.data.close / self.data.volume
-        
-        # buy_rate = self.minvol[0]/self.datas[0].volume > self.p.br = self.lines.PVVPB / self.data.close > 0.9
-        # sell_rate = self.datas[0].volume/self.maxvol[0] > self.p.sr = self.data.close / self.lines.PPVPS > 0.8
-        
-        # buy_rate = self.minvol[0]/self.datas[0].volume = self.lines.PVVPB / self.data.close > self.p.br
-        
-        # sell_rate = self.datas[0].volume/self.maxvol[0] = self.data.close / self.lines.PVVPS > self.p.sr
-        
-        # pv.lines.minvolume * self.data.close / self.data.volume >= self.data_close[0] * self.p.br
-        
-        # buy_rate >= self.params.br and self.data_close[0] <= min_price
-        
-        # self._pvvp.pvvpb >= self.data_close[0] * self.p.br
         
     def next(self):
         sigb = (self.pv.lines.minvolume[0] * self.vpr.lines.vpr[0] >= self.data.close[0]) and (self.mmvp.lines.minvp[0] >= self.data.close[0])
-        sigs = self.data.close[0] >= (self.pv.lines.maxvolume[0] * self.vpr.lines.vpr[0] * self.p.sr) or (self.mmvp.lines.minvp[0] * 2 <= self.lines.vsma[0])
-        if sigb:
-            print(sigb, self.mmvp.lines.minvp[0])
+        sigs = self.data.close[0] >= (self.pv.lines.maxvolume[0] * self.vpr.lines.vpr[0] * self.p.sr) or (self.mmvp.lines.minvp[0] * self.p.rr <= self.lines.vsma[0])
+        # if sigb:
+        #     print(sigb, self.mmvp.lines.minvp[0])
         self.lines.pvvpb[0] = self.data.close[0] if sigb else 0
-        if sigs:
-            print(sigs, self.mmvp.lines.maxvp[0])
+        # if sigs:
+        #     print(sigs, self.mmvp.lines.maxvp[0])
         self.lines.pvvps[0] = self.data.close[0] if sigs else 0
         
         
--- bt/strategies/lightvolume.py
+++ bt/strategies/lightvolume.py
@@ -22,6 +22,7 @@ class LightVolume(bt.Strategy):
     params = (("period",45),
               ('br', 0.90),
               ('sr', 0.80),
+              ('rr', 2),
               ('_pvvp', btind.pvolume.PVVP),
               ('printlog', False),)  # 全局设定交易策略的参数, period是 MA 均值的长度
 
@@ -35,32 +36,8 @@ class LightVolume(bt.Strategy):
         self.data_vol = self.datas[0].volume
         # 初始化交易指令、买卖价格和手续费
         self.order = None
-        self.buy_price = None
-        self.buy_comm = None
-
-        # self.maxvol = bt.talib.MAX(self.data_vol, timeperiod=240)
-        self.minvol = bt.talib.MIN(self.data_vol, timeperiod=self.params.period)
-        self.maxvol = bt.talib.MAX(self.data_vol, timeperiod=self.params.period)
         
-        self.minidx = bt.talib.MININDEX(self.data_vol, timeperiod=self.params.period)
-        self.maxidx = bt.talib.MAXINDEX(self.data_vol, timeperiod=self.params.period)
-        
-        self.min_volume_pos = None
-        self.max_volume_pos = None
-        
-        self._pvvp = btind.pvolume.PVVP(self.data, period=self.p.period, br=self.p.br, sr=self.p.sr)
-
-        # 添加 MinMax 指标，用于计算过去 period 天内的最小/最大成交量
-        # self.volume_minmax = bt.talib.MINMAX(self.data_vol, timeperiod=self.params.period)
-        # self.std_vol_60 = bt.talib.STDDEV(self.data_vol, timeperiod=45,nbdev=1.0)
-        # self.minvol_60 = bt.talib.MIN(self.data_vol, timeperiod=60)
-        # self.minvol_90 = bt.talib.MIN(self.data_vol, timeperiod=90)
-        # self.minvol_120 = bt.talib.MIN(self.data_vol, timeperiod=120)
-        # self.minvol_180 = bt.talib.MIN(self.data_vol, timeperiod=180)
-        # self.minvol_360 = bt.talib.MIN(self.data_vol, timeperiod=360)
-        
-        # self.trade_analyzer = bt.analyzers.TradeAnalyzer()
-        # self._addanalyzer(self.trade_analyzer)
+        self._pvvp = btind.pvolume.PVVP(period=self.p.period, br=self.p.br, sr=self.p.sr, rr=self.p.rr)
 
     def next(self):
         """
@@ -70,65 +47,26 @@ class LightVolume(bt.Strategy):
         # self.log(f'收盘价, {data_close[0]}')  # 记录收盘价
         if self.order:  # 检查是否有指令等待执行,
             return
-        # 检查是否持仓
-        # print(self.vol_sma, self.data_vol[0])
-        buy_rate = self.minvol[0]/self.datas[0].volume
-        sell_rate = self.datas[0].volume/self.maxvol[0]
-        # print(self.datas[0].datetime.date(0), rate , self.data_close[0], rate > 0.9 and 'Y' or '')
-        # print(self.minvol[0], self.minidx[0], self.data_vol.lines[0])
-        # print(self.maxvol[0], self.maxidx[0], self.data_vol[int(self.maxidx[0])])
+        # 检查是否持
         
-        # print(dir(self.minidx))
-        # print(self.volume_minmax.lines.max.buflen())
-
-        # 获取历史最小成交量和对应位置
-        # min_vol = self.volume_minmax.lines.min[0]
-        min_pos = self.minidx[0]#self.volume_minmax.lines.min.idxmin[0]
-
-        # 获取历史最大成交量和对应位置
-        # max_vol = self.volume_minmax.lines.max[0]
-        max_pos = self.maxidx[0]
-        
-        # print(min_pos, max_pos)
-        
-
-        # 根据位置取出历史收盘价
-        history_close = self.data.close.array
-
-        min_price = history_close[int(min_pos)]
-        max_price = history_close[int(max_pos)]
-        
-        history_vol = self.data.volume.array
-
-        min_vol_p = history_vol[int(min_pos)]
-        max_vol_p = history_vol[int(max_pos)]
-
-        # print(f"minidx{min_pos}, min vol {min_vol} {min_vol_p}/{self.minvol[0]}/{self.data_vol[0]}, price {min_price}/{self.data_close[0]}")
-        # print(f"maxidx{max_pos}, max vol {max_vol} {max_vol_p}/{self.maxvol[0]}/{self.data_vol[0]}, price {max_price}/{self.data_close[0]}")
         if not self.position:  # 没有持仓
-            print(self._pvvp.pvvpb[0], '---------')
             # 执行买入条件判断：当日成交量接近目标成交量
             # if buy_rate >= self.params.br and self.data_close[0] <= min_price:
             if self._pvvp.pvvpb > 0:
-                print(self._pvvp.pvvpb/self.data_close[0], 'br-------------', self.data_close[0])
-                self.log("BUY CREATE, %.2f, %.2f %.2f, %.2f" % (self.data_close[0], min_price, buy_rate, sell_rate))
                 # 执行买入
                 self.order = self.buy()
             else:
-                self.log("EMPTY, %.2f, %.2f, %.2f %.2f" % (self.data_close[0], min_price, buy_rate, sell_rate))
+                pass
         else:
             # 执行卖出条件判断：收盘价格跌破15日均线
             # 止盈止损
-            shouldSell = ((self.data_close[0] - self.position.price)/self.position.price < -0.1)# or ((self.data_close[0] - self.position.price)/self.position.price >= 0.5)
-            # print(dir(shouldSell))
-            # if sell_rate >= self.params.sr:# or shouldSell:
+            # shouldSell = ((self.data_close[0] - self.position.price)/self.position.price < -0.1)# or ((self.data_close[0] - self.position.price)/self.position.price >= 0.5)
             if self._pvvp.pvvps[0] > 0:
-                print(self.data_close[0]/self._pvvp.pvvps, 'sr-------------', self.data_close[0])
-                self.log("SELL CREATE, %.2f, %.2f %.2f" % (self.data_close[0], buy_rate, sell_rate))
                 # 执行卖出
                 self.order = self.sell()
             else:
-                self.log("HOLD,%.2f %.2f %.2f" % (self.data_close[0], buy_rate, sell_rate))
+                pass
+                # self.log("HOLD,%.2f %.2f %.2f" % (self.data_close[0], buy_rate, sell_rate))
 
     def log(self, txt, dt=None, do_print=False, do_persistence=False):
         """
